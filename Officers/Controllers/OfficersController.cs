8a6ed42f28e44d814117450f392dcde021e58cd3
Ben Ellis <ben@oldtownit.com>
2013-05-17 23:48:54 -0500

    Wired up some of officer sidebar and form interaction

---
+++ Officers/Controllers/OfficersController.cs
@@ -0,0 +1,37 @@
+﻿using Officers.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Officers.Controllers
+{
+	public class OfficersController : ApiController
+	{
+		public IEnumerable<Officer> Get()
+		{
+			return new Officer[] { new Officer() { Id = 1, FirstName = "David", LastName = "Schlum" }, new Officer() { Id = 2, FirstName = "Tim", LastName = "Franklin"} };
+		}
+
+		public Officer Get(int id)
+		{
+			return new Officer() { Id = 1, FirstName = "David", LastName = "Schlum" };
+		}
+
+		public Officer Post([FromBody]Officer officer)
+		{
+			officer.Id = new Random().Next();
+			return officer;
+		}
+
+		public void Put(int id, [FromBody]Officer value)
+		{
+		}
+
+		public void Delete(int id)
+		{
+		}
+	}
+}
--- Officers/Scripts/app.js
+++ Officers/Scripts/app.js
@@ -2,6 +2,15 @@
 
 $(function () {
 
+    app.OfficerModel = Backbone.Model.extend({
+        urlRoot: 'api/officers'
+    });
+
+    app.OfficerCollection = Backbone.Collection.extend({
+        url: 'api/officers',
+        model: app.OfficerModel
+    });
+
     app.HeroView = Backbone.View.extend({
         el: '#content',
         template: _.template($('#hero').html()),
@@ -17,6 +26,43 @@ $(function () {
         render: function () {
             this.$el.html(this.template());
             return this;
+        },
+        events: {
+            'click #save-button': 'save'
+        },
+        save: function () {
+            var self = this;
+            this.model.set({ 'firstName': $('#firstName').val(), 'lastName': $('#lastName').val() });
+            this.model.save()
+            .done(function () {
+                console.log(self.model);
+                app.officers.add(self.model);
+                Backbone.history.navigate('officers/' + self.model.get('id'), true);
+            });
+        }
+    });
+
+    app.OfficerItemView = Backbone.View.extend({
+        tagName: 'li',
+        template: _.template($('#officer-item-template').html()),
+        initialize: function () {
+            this.listenTo(this.model, 'change', this.render);
+            this.listenTo(this.model, 'destroy', this.remove);
+        },
+        render: function () {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        }
+    });
+
+    app.OfficerListView = Backbone.View.extend({
+        el: '#officer-list',
+        initialize: function () {
+            this.listenTo(app.officers, 'add', this.addOne);
+        },
+        addOne: function (officer) {
+            var itemView = new app.OfficerItemView({ model: officer });
+            $('#officer-list').append(itemView.render().el);
         }
     });
 
@@ -24,7 +70,7 @@ $(function () {
     var AppRouter = Backbone.Router.extend({
         routes: {
             '': 'showHero',
-            'officers/new': 'editOfficer',
+            'officers/new': 'newOfficer',
             'officers/:id': 'displayOfficer',
             'officers/:id/edit': 'editOfficer'
         },
@@ -34,6 +80,11 @@ $(function () {
         },
         displayOfficer: function () {
 
+            console.log('put display here');
+        },
+        newOfficer: function () {
+            var form = new app.OfficerForm({ model: new app.OfficerModel() });
+            form.render();
         },
         editOfficer: function (id) {
             var form = new app.OfficerForm();
@@ -41,9 +92,12 @@ $(function () {
         }
     });
 
+    app.officers = new app.OfficerCollection();
+
+    app.officerListView = new app.OfficerListView();
+    app.officerListView.render();
+
     app.appRouter = new AppRouter();
     Backbone.history.start();
-
-
 
 });
