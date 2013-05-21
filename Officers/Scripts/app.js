b23905a9683ea52c823c39f494281dd452d57d52
Ben Ellis <ben@oldtownit.com>
2013-05-20 21:38:48 -0500

    Initial model & collection with fetch

--- Officers/Scripts/app.js
+++ Officers/Scripts/app.js
@@ -2,8 +2,23 @@
 
 $(function () {
 
-    
+    app.OfficerModel = Backbone.Model.extend({
+        urlRoot: 'api/officers',
+        defaults: {
+            'firstName': '',
+            'lastName': '',
+        }
+    });
+
+    app.OfficerCollection = Backbone.Collection.extend({
+        url: 'api/officers',
+        model: app.OfficerModel
+    });
 
     
+    app.officers = new app.OfficerCollection();
+
+    app.officers.fetch({ reset: true });
+
 
 });
