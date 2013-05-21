2202e45b2bc8dcf60e49a718fef73dae4a7ca666
Ben Ellis <ben@oldtownit.com>
2013-05-20 22:06:43 -0500

    Officer list views

--- Officers/Scripts/app.js
+++ Officers/Scripts/app.js
@@ -15,12 +15,58 @@ $(function () {
         model: app.OfficerModel
     });
 
-    
+    app.HeroView = Backbone.View.extend({
+        el: '#content',
+        template: _.template($('#hero').html()),
+        render: function () {
+            this.$el.html(this.template());
+            return this;
+        }
+    });
+
+    app.OfficerItemView = Backbone.View.extend({
+        tagName: 'li',
+        template: _.template($('#officer-item-template').html()),
+        initialize: function () {
+            this.listenTo(this.model, 'change', this.render);
+            this.listenTo(this.model, 'destroy', this.remove);
+        },
+        render: function () {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        }
+    });
+
+    app.OfficerListView = Backbone.View.extend({
+        el: '#officer-list',
+        initialize: function () {
+            this.listenTo(app.officers, 'add', this.addOne);
+            this.listenTo(app.officers, 'reset', this.addAll);
+        },
+        addOne: function (officer) {
+            var itemView = new app.OfficerItemView({ model: officer });
+            $('#officer-list').append(itemView.render().el);
+        },
+        addAll: function () {
+            $('#officer-list').html('');
+            app.officers.each(this.addOne, this);
+        }
+    });
+
+
     app.officers = new app.OfficerCollection();
+    app.officers.fetch({ reset: true })
+    .done(function () {
 
-    //app.officers.fetch({ reset: true });
+        app.views = {
+            showHero: new app.HeroView(),
+            officerListView: new app.OfficerListView({ collection: app.officers })
+        };
+       
+        app.views.showHero.render();
+        app.views.officerListView.render();
+        app.views.officerListView.addAll();
 
-    // Seed a test record
-    app.officers.create({ firstName: 'David', lastName: 'Schlum', office: 'President' });
+    });
 
 });
